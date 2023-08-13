c1f0db5deebc637111d0d2c7fdbb04e2996905ed
Lynnsanity <lynnellesaavedra8@gmail.com>
2023-08-12 22:15:35 -0500

    some changes for mobile / tablet styling and putting text inside vid

--- svelte-kit/src/routes/+page.svelte
+++ svelte-kit/src/routes/+page.svelte
@@ -1,31 +1,55 @@
 <script>
-  import Navbar from '../lib/components/+navbar.svelte'; // Adjust the path based on your project structure
+  import Navbar from '../lib/components/+navbar.svelte';
 </script>
 <main>
   <Navbar />
-  <section class="flex justify-center items-center bg-zinc-950">
-    <div class="video-container">
-      <video class="responsive-video" muted autoplay loop playsinline disablePictureInPicture>
-        <source src="/umbrella.webm" type="video/webm"/>
-      </video>
-    </div>
+  <section class="flex flex-col shrink-0 video-container">
+    <video class="-z-10 responsive-video" muted autoplay loop playsinline disablePictureInPicture>
+      <source src="/umbrella.webm" type="video/webm"/>
+    </video>
+    <h1 class="text-6xl">Welcome to my website</h1>
+    <button class="text-4xl">Learn More</button>
   </section>
 </main>
+
+<!-- css styling -->
 <style lang="postcss">
   :global(html) {
     background-color: theme(colors.zinc.950);
   }
   .video-container {
-   width: 100%;
-   height: 100%;
-   display: flex;
-   justify-content: center; /* Horizontal centering */
-   align-items: center; /* Vertical centering */
+    position: relative;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
   }
   .responsive-video {
-    position: center;
-    width: 98%;
-    height: 30%;
-    border-radius: 5px; /* Adjust the value to control the amount of rounding */ 
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  /* tablet styles */
+  @media (max-width: 1024px) {
+    .responsive-video {
+      width: 100%;
+      height: 100%;
+      transform: translateX(-15%) scale(1.5);
+      transform-origin: left center;
+    }
+  }
+
+  /* mobile styles */
+  @media (max-width: 768px) {
+    .responsive-video {
+      width: 100%;
+      height: 100%;
+      transform: translateX(-15%) scale(1.5); 
+      transform-origin: left center; 
+    }
   }
 </style>
